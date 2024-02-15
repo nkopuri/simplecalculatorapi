e84461e1ad92d1628a2b52e8fd5d4a204a121120
Nutan Kopuri <nutankopuri@Nutans-MacBook-Air.local>
2024-02-15 10:53:13 -0600

    second commit

--- calculatorAPI.py
+++ calculatorAPI.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Welcome to my calculator'
+    return 'Welcome to my simple calculator'
 def add2num(x, y):
     return (x + y)
 
